d8221b91cb0c8e217f33f5a12a66d5c6c3b2a262
CCandYY <stephy.lily.y@gmail.com>
2023-07-31 18:01:21 +0800

    大致框架

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+//全局css
 import '@/assets/global.css';
+//图标
+import IconSvg from '@/components/icon-component'
+//axios
+// import axios from 'axios';
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI);
+Vue.use(ElementUI);//ui
+Vue.component('icon-svg', IconSvg)//icon
+// Vue.prototype.$axios='axios'
+
 
 new Vue({
   router,
